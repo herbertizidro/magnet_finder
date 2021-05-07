a4f50753b8357565899d5d9ce516ef1c130379d5
Herbert Izidro <herbertsouza80@yahoo.com>
2021-05-07 19:02:25 -0300

    comentários adicionados

--- script.js
+++ script.js
@@ -3,26 +3,26 @@ document.addEventListener('DOMContentLoaded', function(){
 	
 	document.querySelector('#btn').addEventListener('click', function(){
 		let select = document.getElementById('option');
-		let select_value = select.options[select.selectedIndex].value;
+		let select_value = select.options[select.selectedIndex].value; // opção escolhida (audio, video, games e etc)
 
-		document.getElementById('status-busca').style.display = 'block';
-		document.getElementById('erro').style.display = 'none';
-		document.querySelector('#status-busca').innerHTML = '<div class="d-flex justify-content-center"><div class="spinner-border" style="color: #FF1616; width: 2rem; height: 2rem;" role="status"><span class="sr-only">Loading...</span></div></div>';
-		let pesquisa = document.querySelector('#termo-pesquisa').value;
+		document.getElementById('status-busca').style.display = 'block'; // mostra a div
+		document.getElementById('erro').style.display = 'none'; // esconde a div
+		document.querySelector('#status-busca').innerHTML = '<div class="d-flex justify-content-center"><div class="spinner-border" style="color: #FF1616; width: 2rem; height: 2rem;" role="status"><span class="sr-only">Loading...</span></div></div>'; // loading spinner
+		let pesquisa = document.querySelector('#termo-pesquisa').value; // pega o termo de pesquisa
 		try{
 
-			if(pesquisa.length == 0){
+			if(pesquisa.length == 0){ // caso clique em "pesquisar" sem ter colocado o termo antes
 				document.getElementById('status-busca').style.display = 'none';
 				document.getElementById('erro').style.display = 'block';
 				document.querySelector('#erro').innerHTML = '<div class="alert rounded-0 alert-danger text-center" role="alert">&#10060;&nbsp;&nbsp; Ops, nothing to search! Enter the name of a movie, music, software ... </div>';
 			}else{
 				
-				let url = `https://api.sumanjay.cf/torrent/?query=${pesquisa}`;							
+				let url = `https://api.sumanjay.cf/torrent/?query=${pesquisa}`;	// url da API					
 				
-				fetch(url).then(res => {
+				fetch(url).then(res => { // faz a requisição e obtem um json
 						return res.json();
 					}).then(json => {
-						try{
+						try{ // verifica se o json tem algum conteúdo e gera a tabela(<table>)
 							if(json.length > 0 ){
 								tabela = '<table class="table"><thead><tr><th scope="col">Results for: ' + pesquisa + '</th></thead><tbody>'
 								for (let j = 0; j < json.length; j++){
