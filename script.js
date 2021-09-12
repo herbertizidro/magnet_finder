ca21849ff22909b43047d56cfd1279eaf985c8ca
Herbert Izidro <herbertsouza80@yahoo.com>
2021-09-12 20:18:45 -0300

    versão 0.6
    1. filtros de busca agora abrangem uma quantidade maior de resultados
    2. agora a idade e o site origem do arquivos também são informados

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-
 document.addEventListener('DOMContentLoaded', function(){ // aguarda o documento HTML estar completamente carregado
 	
 	// spinner e mensagens de erro
 	const spinner = '<div class="d-flex justify-content-center"><div class="spinner-border" style="color: #FF1616; width: 2rem; height: 2rem;" role="status"><span class="sr-only">Loading...</span></div></div>'; // loading spinner 
-	const erro_termo_ausente = '<div class="alert rounded-0 alert-danger text-center" role="alert">&#10060;&nbsp;&nbsp; Ops, nothing to search! Enter the name of a movie, music, software ... </div>'; // mensagem de erro 
+	const erro_termo_ausente = '<div class="alert rounded-0 alert-danger text-center" style="height: 33px;" role="alert">&#10060;&nbsp;&nbsp; Ops, nothing to search! Enter the name of a movie, music, software ... </div>'; // mensagem de erro 
 	// pra quando o usuário tenta pesquisar sem inserir um termo(nome de filme, música e etc)
-	const erro_nao_encontrado = '<div class="alert rounded-0 alert-danger text-center" role="alert">Sorry, no result found! &#128557;</div>';
-	const erro_interno = '<div class="alert rounded-0 alert-danger text-center" role="alert">&#10060;&nbsp;&nbsp; The search could not be completed, please try again later.</div>';
+	const erro_nao_encontrado = '<div class="alert rounded-0 alert-danger text-center" style="height: 33px;" role="alert">Sorry, no result found! &#128557;</div>';
+	const erro_interno = '<div class="alert rounded-0 alert-danger text-center" style="height: 33px;" role="alert">&#10060;&nbsp;&nbsp; The search could not be completed, please try again later.</div>';
 	
 	// funções pra controlar o conteúdo exibido(mensagens de erro ou resultados de pesquisa)
 	function mostrarEsconderDiv(id, estilo){
@@ -26,7 +25,22 @@ document.addEventListener('DOMContentLoaded', function(){ // aguarda o documento
 	// aguarda um evento clique no botão "pesquisar"
 	document.querySelector('#btn').addEventListener('click', function(){
 		let select = document.getElementById('option');
-		let select_value = select.options[select.selectedIndex].value; // opção escolhida (audio, video, games e etc)
+		let select_item = select.options[select.selectedIndex].value; // opção escolhida (audio, video, games e etc)
+		let select_value;
+		
+		if(select_item == ""){
+			select_value = [""]
+		}else if(select_item == "Video"){
+			select_value = ["Movie", "Video", "TV", "Show"]
+		}else if(select_item == "Audio"){
+			select_value = ["Music", "Audio"]
+		}else if(select_item == "Applications"){
+			select_value = ["Application", "App", "Software"]
+		}else if(select_item == "Games"){
+			select_value = ["Games"]
+		}else{
+			select_value = ["Other"]
+		}
 		
 		exibeConteudoDiv(spinner, 'erro', 'status-busca') // coloca o spinner bootstrap na div "status-busca"
 
@@ -37,7 +51,7 @@ document.addEventListener('DOMContentLoaded', function(){ // aguarda o documento
 				exibeConteudoDiv(erro_termo_ausente, 'status-busca', 'erro') // o erro que será exibido, a div que será escondida e a div que será exibida
 			}else{
 				
-				let url = `https://api.sumanjay.cf/torrent/?query=${pesquisa}`;	// url da API + o termo
+				let url = `https://sumanjay.up.railway.app/torrent/?query=${pesquisa}`;	// url da API + o termo
 				
 				fetch(url).then(res => { // faz a requisição e obtem um json
 						return res.json();
@@ -45,10 +59,16 @@ document.addEventListener('DOMContentLoaded', function(){ // aguarda o documento
 						try{ // verifica se o json tem algum conteúdo e gera a tabela(<table>)
 							if(json.length > 0 ){
 								// monta a tabela com os resultados da busca
-								tabela = '<table class="table"><thead><tr><th scope="col">Results for: ' + pesquisa + '</th></thead><tbody>'
+								if(select_item == ""){
+									tabela = '<table class="table"><thead><tr><th scope="col">Results for ' + pesquisa + ' in category All:</th></thead><tbody>'
+								}else{
+									tabela = '<table class="table"><thead><tr><th scope="col">Results for ' + pesquisa + ' in category ' + select_item + ':</th></thead><tbody>'	
+								}
 								for (let j = 0; j < json.length; j++){
-									if(json[j]["type"].indexOf(String(select_value)) != -1){ // filtra os resultados de acordo com a categoria escolhida no select
-										tabela = tabela + '<tr><td>' + '<a href="' + json[j]['url'] +'" target="_blank">' + json[j]['name'] + '</a><br> Size: ' + json[j]['size'] + '&nbsp;&nbsp;|&nbsp;&nbsp;Seeder: ' + json[j]['seeder'] + '&nbsp;&nbsp;|&nbsp;&nbsp;Leecher: ' + json[j]['leecher'] + '&nbsp;&nbsp;|&nbsp;&nbsp;Type: ' + json[j]['type'] + '&nbsp;&nbsp;|&nbsp;&nbsp;<a href="' + json[j]['magnet'] + '" target="_blank">Download</a></td></tr>'
+									for (let l = 0; l < select_value.length; l++){
+										if(json[j]["type"].indexOf(String(select_value[l])) != -1){ // filtra os resultados de acordo com a categoria escolhida no select
+											tabela = tabela + '<tr><td>' + '<a href="' + json[j]['url'] +'" target="_blank">' + json[j]['name'] + '</a><br> Size: ' + json[j]['size'] + '&nbsp;&nbsp;|&nbsp;&nbsp;Seeder: ' + json[j]['seeder'] + '&nbsp;&nbsp;|&nbsp;&nbsp;Leecher: ' + json[j]['leecher'] + '&nbsp;&nbsp;|&nbsp;&nbsp;Type: ' + json[j]['type'] + '&nbsp;&nbsp;|&nbsp;&nbsp;Age: ' + json[j]['age'] + '&nbsp;&nbsp;|&nbsp;&nbsp;Site: ' + json[j]['site'] +'&nbsp;&nbsp;|&nbsp;&nbsp;<a href="' + json[j]['magnet'] + '" target="_blank">Download</a></td></tr>'
+										}
 									}
 								}							
 								tabela = tabela + '</tbody></table>'
